971407152baa6a1901e4db089d9e076ce0b8004d
Andrés Moya <andres.moya@kaleidos.net>
2014-10-11 19:11:44 +0200

    Cause 404 instead of 500 for an unknown page

--- src/webapp/plain_pages/views.py
+++ src/webapp/plain_pages/views.py
@@ -1,7 +1,12 @@
 # -*- encoding: utf-8 -*-
 
 from django.shortcuts import render_to_response
+from django.template.base import TemplateDoesNotExist
+from django.http import Http404
 
 def statichtml(request, html_id='index'):
-    return render_to_response('webapp/plain_pages/' + html_id + '.html')
+    try:
+        return render_to_response('webapp/plain_pages/' + html_id + '.html')
+    except TemplateDoesNotExist:
+        raise Http404('No se encuentra el documento %s' % html_id)
 
