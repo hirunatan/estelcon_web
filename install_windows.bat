57f95c1131fc093c84a036c13a7b355fa6f9fcb5
Andrés Moya <andres.moya@kaleidos.net>
2015-06-06 11:45:36 +0200

    Fix installation in windows

--- install_windows.bat
+++ install_windows.bat
@@ -1,7 +1,6 @@
 @echo off
-set PYTHON_DIR="C:\WinPython-32bit-2.7.9.5"
-set PATH=%PYTHON_DIR%;%PATH%
 pip install -r requirements.txt
 copy src\settings\local.py.example src\settings\local.py
+copy db.sqlite3.example db.sqlite3
 pause
 
--- runserver.bat
+++ runserver.bat
@@ -1,6 +1,4 @@
 @echo off
-set PYTHON_DIR="C:\WinPython-32bit-2.7.9.5"
-set PATH=%PYTHON_DIR%;%PATH%
 cd src
 python manage.py runserver
 pause
