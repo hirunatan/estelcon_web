26c8768dbffd799ecfb18ac12162203fcc21a662
Andrés Moya <andres.moya@kaleidos.net>
2015-06-06 11:16:43 +0200

    Prepare for WinPython

--- install_windows.bat
+++ install_windows.bat
@@ -1,4 +1,6 @@
 @echo off
+set PYTHON_DIR="C:\WinPython-32bit-2.7.9.5"
+set PATH=%PYTHON_DIR%;%PATH%
 pip install -r requirements.txt
 copy src\settings\local.py.example src\settings\local.py
 pause
--- runserver.bat
+++ runserver.bat
@@ -1,4 +1,6 @@
 @echo off
+set PYTHON_DIR="C:\WinPython-32bit-2.7.9.5"
+set PATH=%PYTHON_DIR%;%PATH%
 cd src
 python manage.py runserver
 pause
